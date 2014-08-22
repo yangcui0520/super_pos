838bca6d6b4cbbc43e8e9c0a6c4806a6679cc974
yangcui0520 <1205120574@qq.com>
2014-08-22 17:28:12 +0800

    get the right total price

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -43,10 +43,48 @@ function printInventory(inputs) {
             item.price = new_inputs[f].price;
             item.unit = new_inputs[f].unit;
             item.count = 1;
+            item.mot =0;
             new_inputsCount.push(item);
         }
     }
-    console.info(JSON.stringify(new_inputsCount))
+    console.info(JSON.stringify(new_inputsCount));
+    for(var i= 0;i<new_inputsCount.length;i++){
+        var prom=[
+            'ITEM000000',
+            'ITEM000001',
+            'ITEM000005'
+        ];
+        for(var k=0;k<prom.length;k++){
+          if (prom[k] == new_inputsCount[i].barcode ){
+              var pro_count=new_inputsCount[i].count;
+              var mot=Math.floor(pro_count/3);
+              new_inputsCount[i].mot=mot;
+          }
+        }
+    }
+    
+    var array_print = "";
+    var total_price = 0;
+    for (var f = 0; f < new_inputsCount.length; f++) {
+        var print_format =
+            '名称：' + new_inputsCount[f].name +
+            '，数量：' + new_inputsCount[f].count + new_inputsCount[f].unit +
+            '，单价：' + new_inputsCount[f].price +
+            '.00(元)，小计：' + new_inputsCount[f].price * (new_inputsCount[f].count-new_inputsCount[f].mot) + '.00(元)\n';
+
+        array_print = array_print + print_format;
+
+        var num = new_inputsCount[f].price * (new_inputsCount[f].count-new_inputsCount[f].mot);
+        total_price = total_price + num;//迭代计算总价。
+    }
+    var expectText =
+        '***<没钱赚商店>购物清单***\n' +
+        array_print +
+        '----------------------\n' +
+        '总计：' + total_price + '.00(元)\n' +
+        '**********************';
+    console.info(expectText);
+    console.log(expectText);
 }
 /*
 function loadPromotions() {
